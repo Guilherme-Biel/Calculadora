898667a322fc0748e907221724d2463d1e48aa1d
Guilherme-Biel <gui.gabriel2010@hotmail.com>
2024-12-11 23:13:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var n1 = document.querySelector('#n1')
+var n2 = document.querySelector('#n2')
+var resultado = document.querySelector('span')
+
+function somar() {
+    resultado.innerHTML = Number(n1.value) + Number(n2.value)
+}
+function subtrair() {
+    resultado.innerHTML = Number(n1.value) - Number(n2.value)
+}
+function multiplicar() {
+    resultado.innerHTML = Number(n1.value) * Number(n2.value)
+}
+function dividir() {
+    resultado.innerHTML = Number(n1.value) / Number(n2.value)
+}
